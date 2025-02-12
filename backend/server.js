4e88442af7da89dd5e2bcaf61df5bb12fc0d78b6
SetaKaoru <rhysdarys@gmail.com>
2025-02-12 21:44:58 +0800

    huh

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,15 @@ const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
 
+const cors = require("cors");
+
 // express app
-const app = express()
+const app = express();
+
+app.use(cors());
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
